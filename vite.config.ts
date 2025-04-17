3609a7527a44b7c672932a8da2ba0e8fae898405
Johanna1506 <johanna.jato@gmail.com>
2025-04-17 17:18:01 +0200

    fix
    fix
    
    fix
    
    fix

--- vite.config.ts
+++ vite.config.ts
@@ -17,4 +17,15 @@ export default defineConfig({
     },
   },
   base: '/cake-rate/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
 })
