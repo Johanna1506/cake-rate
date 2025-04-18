3abdd563f38b004b396a5faf222016128528aa03
Johanna1506 <johanna.jato@gmail.com>
2025-04-18 10:20:30 +0200

    fix

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@components': path.resolve(__dirname, './src/components'),
+            '@pages': path.resolve(__dirname, './src/pages'),
+            '@hooks': path.resolve(__dirname, './src/hooks'),
+            '@services': path.resolve(__dirname, './src/services'),
+            '@lib': path.resolve(__dirname, './src/lib'),
+            '@context': path.resolve(__dirname, './src/context'),
+        },
+    },
+    base: '/cake-rate/',
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        emptyOutDir: true,
+        sourcemap: true,
+        rollupOptions: {
+            input: path.resolve(__dirname, './index.html'),
+            output: {
+                assetFileNames: 'assets/[name]-[hash][extname]',
+                chunkFileNames: 'assets/[name]-[hash].js',
+                entryFileNames: 'assets/[name]-[hash].js',
+            },
+        },
+    },
+});
