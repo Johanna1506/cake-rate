2fcdff600878157b51200699809315e501faf114
Johanna1506 <johanna.jato@gmail.com>
2025-04-24 12:22:10 +0200

    fix: react create root

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ThemeProvider } from '@mui/material/styles'
@@ -8,16 +8,22 @@ import App from './App'
 import { lightTheme } from './theme/theme'
 import { queryClient } from './lib/queryClient'
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const container = document.getElementById('root');
+if (!container) throw new Error('Failed to find the root element');
+const root = createRoot(container);
+
+const Main: React.FC = () => (
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </ThemeProvider>
+  </QueryClientProvider>
+);
 
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={lightTheme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <Main />
   </React.StrictMode>
 );
